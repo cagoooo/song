5737ca95f54a1e7b28b7133e6b3d5615fa798b74
ipad4 <21751599-ipad4@users.noreply.replit.com>
2025-01-12 04:23:10 +0000

    Restored to '69a446f13489362af2ead8607fca07cb5dec5d68'
    Replit-Restored-To:69a446f13489362af2ead8607fca07cb5dec5d68

--- client/src/components/ui/toaster.tsx
+++ client/src/components/ui/toaster.tsx
@@ -7,29 +7,26 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
-import { AnimatePresence } from "framer-motion"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      <AnimatePresence mode="popLayout">
-        {toasts.map(function ({ id, title, description, action, ...props }) {
-          return (
-            <Toast key={id} {...props}>
-              <div className="grid gap-1">
-                {title && <ToastTitle>{title}</ToastTitle>}
-                {description && (
-                  <ToastDescription>{description}</ToastDescription>
-                )}
-              </div>
-              {action}
-              <ToastClose />
-            </Toast>
-          )
-        })}
-      </AnimatePresence>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
       <ToastViewport />
     </ToastProvider>
   )
